0fa8c91e4bb58c607b9053ce840a2eb8081fa09b
Patrick Klitzke <philologos14@gmail.com>
2016-02-03 17:31:55 +0900

    example of navigator

--- examples/react/navigator/app.jsx
+++ examples/react/navigator/app.jsx
@@ -5,13 +5,25 @@ var MyPage = React.createClass({
   pushPage : function() {
    this.props.pushPage();
   },
+  popPage : function() {
+   this.props.popPage();
+  },
+
   render: function() {
+
+    var popButton;
+
+    if (this.props.popPage) { 
+      popButton =<ons-button style={{marginRight: 10}} onClick={this.popPage}> Pop </ons-button>
+    }
+
     return <OnsPage>
       <OnsToolbar>
         <div className="center"> {this.props.title} </div>
       </OnsToolbar>
       <div style={{display: 'flex'}}> 
         <div style={{flex: 1}} />
+        {popButton}
         <ons-button onClick={this.pushPage}> Push </ons-button>
         <div style={{flex: 1}} />
       </div>
@@ -19,28 +31,38 @@ var MyPage = React.createClass({
   }
 });
 
+
 var MyNav  = React.createClass({
   getInitialState: function() {
-    return { };
+    return {};
+  },
+
+
+  popPage: function() {
+
+    this.counter--;
+    this.refs.navi.popPage();
   },
 
   pushPage: function() {
     //console.log('push page');
+    // this.setState({navNumber: this.state.navNumber +1});
+
+    this.counter++;
+    var navTitle = "Navigator "+ this.counter;
+
     this.refs.navi.pushComponent(
-      <MyPage title="Navigator2" pushPage={this.pushPage} />
+      <MyPage title={navTitle} popPage={this.popPage} pushPage={this.pushPage} />
     );
   },
 
   componentDidMount: function() {
-    // console.log('hello');
-    // console.log(this.refs.navigator);
+    this.counter = 1;
   },
   
   render: function() {
-    // console.log('render');
-    // console.log(this.refs.navigator);
     return <OnsNavigator ref="navi">
-      <MyPage title="Navigator" pushPage={this.pushPage} />
+      <MyPage title="Navigator 1" pushPage={this.pushPage} />
     </OnsNavigator>
   }
 });
